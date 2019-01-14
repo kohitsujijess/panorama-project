8f2b8f6e851e2ea6175672d67846dcff511c1bdf
Liko <liko@LikonoMacBook-ea.local>
2019-01-15 00:28:41 +0900

    edited take picture function

--- App.js
+++ App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PanoramaImage from './src/components/PanoramaImage';
 
+if (typeof window !== 'object') {
+  global.window = global;
+  global.window.navigator = {};
+}
+
 export default class App extends React.Component {
+
+
   render() {
     return (
       <PanoramaImage />
--- src/components/PanoramaImage.js
+++ src/components/PanoramaImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, CameraRoll, View, TouchableOpacity, Image } from 'react-native';
 import { Camera, Permissions, Icon } from 'expo';
-import mergeImages from 'merge-images';
+import Canvas, {Image as  ImageData}  from 'react-native-canvas';
 const pictures = [];
 
 export default class PanoramaImage extends React.Component {
@@ -10,6 +10,8 @@ export default class PanoramaImage extends React.Component {
     this.state = {
       hasCameraPermission: null,
       type: Camera.Constants.Type.back,
+      cameraRollUri: null,
+      pictureList: null,
     };
 
     this.takePicture = this.takePicture.bind(this);
@@ -20,6 +22,8 @@ export default class PanoramaImage extends React.Component {
     this.setState({
       hasCameraPermission: status === 'granted',
     });
+    const { camera_roll_status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraRollPermission: camera_roll_status === 'granted' });
   }
 
   async takePicture() {
@@ -27,19 +31,14 @@ export default class PanoramaImage extends React.Component {
       let pictureData = await this.camera.takePictureAsync();
       pictures.push(pictureData.uri);
     }
-    // TODO 実機を矢印の方向に動かすというメッセージを表示
 
-    let interVal = setInterval(take, 1000);
+    let interVal = setInterval(take, 2000);
     setTimeout(function(){
       clearInterval(interVal);
     }, 10000);
 
-    const mergePictures = () => {
-      console.log(pictures);
-      mergeImages(pictures).then(b64 => { 
-        console.log(b64);
-        CameraRoll.saveToCameraRoll(b64, ['photo']);
-      });
+    const mergePictures = async () => {
+      this.setState({ pictureList: pictures });
     }
      
     const delayRun = (waitSeconds, someFunction) => {
@@ -51,6 +50,28 @@ export default class PanoramaImage extends React.Component {
     }
      
     delayRun(12000, mergePictures);
+  }
+
+  handleCanvas(canvas) {
+    canvas.width = 200;
+    canvas.height = 200;
+
+    const context = canvas.getContext('2d');
+    context.fillRect(0, 0, 200, 200);
+
+    context.getImageData(0, 0, 200, 200)
+      .then(async (imageData) => {
+        const data = Object.values(imageData.data);
+        const length = Object.keys(data).length;
+        for (let i = 0; i < length; i += 4) {
+          data[i] = 0;
+          data[i + 1] = 0;
+          data[i + 2] = 0;
+        }
+        const imgData = new ImageData(canvas, data, 200, 200);
+        //context.putImageData(imgData, 0, 0);
+        //let saveResult = await CameraRoll.saveToCameraRoll(imgData, 'photo'); 第一引数が'pictureData.uri'みたいにstringになる
+      });
   }
 
   render() {
@@ -100,14 +121,45 @@ export default class PanoramaImage extends React.Component {
               }}
             >
               <Icon.MaterialIcons
+                name="arrow-forward"
+                size={70}
+                style={{ marginBottom: 20 }}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                alignSelf: 'flex-end',
+                alignItems: 'left',
+              }}
+              onPress={() => {
+                this.takePicture();
+              }}
+            >
+              <Icon.MaterialIcons
                 name="camera"
                 size={70}
                 style={{ marginBottom: 20 }}
               />
             </TouchableOpacity>
+
           </View>
         </Camera>
-      </View>
-    );
+        {this.state.pictureList && 
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center',
+        alignItems: 'center'}}>
+        {this.state.pictureList && 
+          this.state.pictureList.map((item, index) => (
+            <Image
+          key={index}
+          source={{ uri: item }}
+          style={{ width: 80, height: 80 }}
+         />
+        ))}
+        </View>
+        }
+        </View>
+        );
   }
 }
